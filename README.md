f81243374c2df1cfa48c5f28149c558873d0c993
kyokutyo <haruta@kyokutyo.com>
2014-06-04 01:39:52 +0900

    2014.6.4 版

---
+++ README.md
@@ -0,0 +1,40 @@
+これまでに夫婦で泊まった都道府県
+================================
+
+![開発中の画面キャプチャ](https://dl.dropboxusercontent.com/u/343/travel-memo.png)
+
+開発
+----
+
+    $ cd path/to/nakayoshi
+    $ bundle exec middleman server
+
+本番反映
+--------
+
+    $ bundle exec middleman build
+
+(build後、middleman-syncによってS3に反映される)
+
+今後やろうかと思ってること
+--------------------------
+
+* [Issues](https://github.com/kyokutyo/nakayoshi/issues)
+
+使ってるもの
+------------
+
+* [middleman/middleman](https://github.com/middleman/middleman)
+* [Slim](http://slim-lang.com/)
+* [yterajima/middleman-slim](https://github.com/yterajima/middleman-slim)
+* [jquery/jquery](https://github.com/jquery/jquery)
+* [jquery-color-utils](https://code.google.com/p/jquery-color-utils/)
+* [Pure](http://purecss.io/)
+* [yterajima/middleman-pure](https://github.com/yterajima/middleman-pure)
+* [Google Fonts Nunito](http://www.google.com/fonts/specimen/Nunito)
+* [Google Charts](https://developers.google.com/chart/?hl=ja)
+* [Google Analytics](http://www.google.co.jp/intl/ja/analytics/)
+* [MrJoy/middleman-google-analytics](https://github.com/MrJoy/middleman-google-analytics)
+* [karlfreeman/middleman-sync](https://github.com/karlfreeman/middleman-sync)
+* [S3](http://aws.amazon.com/jp/s3/)
+* [Route53](http://aws.amazon.com/jp/route53/)
---
+++ source/javascripts/all.js
@@ -0,0 +1,71 @@
+$(function() {
+    "use strict";
+    var $items = $('.content li'),
+        $visited_items = $items.filter('.v'),
+        $next_items = $items.filter('.n'),
+        $message_box = $('#message'),
+        $percentage_box = $('#percentage'),
+        $btn_filter_visited = $('#filter_v'),
+        $btn_filter_next = $('#filter_n'),
+        is_smartphone = !$('.sidebar footer').is(':visible');
+
+    // message
+    $visited_items.hover(function() {
+        var $t = $(this);
+        $message_box.find('p').html($t.data('msg')).end()
+                    .find('.date span').html($t.data('date')).end()
+                    .show();
+    },
+    function() {
+        $message_box.hide();
+    });
+
+    // percentage
+    var percentage = Math.floor($visited_items.length/$items.length * 100),
+        percentage_box_width = $percentage_box.width() + 17, // 余白があるので大きめの画像を取得
+        bg_color = $.fmtColor($('body').css("background-color")).replace('#','');
+    $percentage_box.html('<strong>' + $visited_items.length + '</strong>/' + $items.length)
+                       .css('background-image', 'url("http://chart.apis.google.com/chart?cht=p&chd=t:' + percentage + ',' + (100 - percentage) + '&chs=' + percentage_box_width + 'x' + percentage_box_width + '&chco=' + bg_color + '")');
+
+    // filter_visited
+    $btn_filter_visited.on('click', function() {
+        var $t = $(this),
+            $li = $t.closest('li');
+        if($li.hasClass('on')) {
+            $items.show();
+            $li.removeClass('on')
+               .find('i').attr('class', 'icon-check-empty');
+
+        } else {
+            $items.not($visited_items).hide();
+            $li.addClass('on')
+               .find('i').attr('class', 'icon-check');
+            $('body').scrollTop(0);
+        }
+    })
+    .on('mouseover', function() {
+        var $t = $(this);
+        if($t.closest('li').hasClass('on')) {
+            $t.find('i').attr('class', 'icon-check-empty');
+        } else {
+            $t.find('i').attr('class', 'icon-check');
+        }
+    })
+    .on('mouseout', function() {
+        var $t = $(this);
+        if($t.closest('li').hasClass('on')) {
+            $t.find('i').attr('class', 'icon-check');
+        } else {
+            $t.find('i').attr('class', 'icon-check-empty');
+        }
+    });
+
+    // smartphone
+    if(is_smartphone) {
+        $items.not($visited_items).hide();
+        $('.sidebar footer').clone()
+                            .addClass('pure-hidden-tablet')
+                            .addClass('pure-hidden-desktop')
+                            .appendTo($('.content'));
+    }
+});
