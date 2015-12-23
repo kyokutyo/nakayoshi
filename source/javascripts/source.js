b2e8e03a489aa4b76b1303c8f8c9aeb1d58a6ae2
kyokutyo <haruta@kyokutyo.com>
2015-12-23 15:28:00 +0900

    lint 通らなかったので修正する

--- source/javascripts/source.js
+++ source/javascripts/source.js
@@ -1,14 +1,14 @@
 $(() => {
     "use strict";
 
-    const $items              = $('.content li');
-    const $visited_items      = $items.filter('.v');
-    const $next_items         = $items.filter('.n');
-    const $message_box        = $('#message');
-    const $percentage_box     = $('#percentage');
+    const $items = $('.content li');
+    const $visited_items = $items.filter('.v');
+    const $next_items = $items.filter('.n');
+    const $message_box = $('#message');
+    const $percentage_box = $('#percentage');
     const $btn_filter_visited = $('#filter_v');
-    const $btn_filter_next    = $('#filter_n');
-    const is_smartphone       = !$('.sidebar footer').is(':visible');
+    const $btn_filter_next = $('#filter_n');
+    const is_smartphone = !$('.sidebar footer').is(':visible');
 
     // message
     $visited_items.hover(function() {
@@ -23,16 +23,16 @@ $(() => {
     });
 
     // percentage
-    const percentage           = Math.floor($visited_items.length/$items.length * 100);
+    const percentage = Math.floor($visited_items.length/$items.length * 100);
     const percentage_box_width = $percentage_box.width() + 17; // 余白があるので大きめの画像を取得
-    const bg_color             = $.fmtColor($('body').css("background-color")).replace('#','');
+    const bg_color = $.fmtColor($('body').css("background-color")).replace('#','');
 
     $percentage_box.html('<strong>' + $visited_items.length + '</strong>/' + $items.length)
                    .css('background-image', 'url("http://chart.apis.google.com/chart?cht=p&chd=t:' + percentage + ',' + (100 - percentage) + '&chs=' + percentage_box_width + 'x' + percentage_box_width + '&chco=' + bg_color + '")');
 
     // filter_visited
     $btn_filter_visited.on('click', function() {
-        const $t  = $(this);
+        const $t = $(this);
         const $li = $t.closest('li');
 
         if ($li.hasClass('on')) {
